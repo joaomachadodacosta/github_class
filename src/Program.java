5f188cb8dfe79235cd89561276b3ecf9ccdf1ef7
joaomachadodacosta <joaomachadodacosta@gmail.com>
2021-01-11 15:36:50 +0000

    Add boa tarde

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,7 @@
 public class Program {
     public static void main(String[] args) {
         System.out.println("Bom dia!");
+        System.out.println("Boa tarde!");
+        System.out.println("Boa noite!");
     }
 }
